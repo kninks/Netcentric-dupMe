1977ffe4457d815cefe3b2b1e07e2465c08a7d7f
kninks <kankanitsuppa@gmail.com>
2024-09-02 22:23:00 +0200

    feat: set up ci-cd .yml files

--- client/src/__test__/pages/Home/Home.test.tsx
+++ client/src/__test__/pages/Home/Home.test.tsx
@@ -4,7 +4,6 @@ import { render, screen } from '@testing-library/react';
 import Home from '../../../pages/Home/Home';
 import { MemoryRouter } from "react-router-dom";
 
-
 describe('renders Home page correctly', () => {
     it('should render initial components correctly', () => {
         render(
