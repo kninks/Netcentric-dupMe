ce03bc3b03b170bc369c0ce6120b8b4dc434aae8
kninks <kankanitsuppa@gmail.com>
2024-09-02 21:44:41 +0200

    feat: setup libs and dummy tests

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import "./App.css";
 
 import Room from "./pages/Room/Room";
@@ -10,18 +10,20 @@ import HowToPlay from "./pages/HowToPlay/HowToPlay";
 
 function App() {
     return (
-        <div className="App">
-            <div className="tab">DupMe</div>
-            <div className="page">
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/room" element={<Room />} />
-                <Route path="/how-to-play" element={<HowToPlay />} />
-                <Route path="/chat" element={<ChatRoom />} />
-                <Route path="/support-us" element={<SupportUs />} />
-            </Routes>
+        <BrowserRouter>
+            <div className="App">
+                <div className="tab">DupMe</div>
+                <div className="page">
+                    <Routes>
+                        <Route path="/" element={<Home/>}/>
+                        <Route path="/room" element={<Room/>}/>
+                        <Route path="/how-to-play" element={<HowToPlay/>}/>
+                        <Route path="/chat" element={<ChatRoom/>}/>
+                        <Route path="/support-us" element={<SupportUs/>}/>
+                    </Routes>
+                </div>
             </div>
-        </div>
+        </BrowserRouter>
     );
 }
 
